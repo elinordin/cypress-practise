8c9ae00d3f2a348fd92226642258bec7d4c33c20
elinordin <elin.evelina@live.se>
2021-08-17 15:16:12 +0200

    auth.js setup

--- project-3-auth-api/index.js
+++ project-3-auth-api/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const authRoute = require('./routes/auth')
+
+app.use('/api/user', authRoute)
 
 app.listen(3000, () => {
     console.log('Server is up and running on port 3000')
