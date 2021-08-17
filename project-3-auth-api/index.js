01843a00954b7ddcf22f605d88b8d5f3a1dee0b0
elinordin <elin.evelina@live.se>
2021-08-17 17:01:53 +0200

    database setup including mongoose and dotenv

--- project-3-auth-api/index.js
+++ project-3-auth-api/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const authRoute = require('./routes/auth')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+mongoose.connect(
+    process.env.DB_CONNECT,
+    {useNewUrlParser: true}, 
+    () => {console.log('Successfully connected to database')})
 
 app.use('/api/user', authRoute)
-
 app.listen(3000, () => {
     console.log('Server is up and running on port 3000')
 })
