0a86d7a609e88025e44db89fc87f1cff49652cca
elinordin <elin.evelina@live.se>
2021-08-17 15:56:35 +0200

    install mongoose, run tests in the command line

--- project-3-auth-api/cypress/integration/auth.spec.js
+++ project-3-auth-api/cypress/integration/auth.spec.js
@@ -1,10 +1,8 @@
-const { response } = require("express")
-
-it('returns register with post-request to /register', () => {
+it('returns 200 with post-request to /register', () => {
 
     cy.request('POST', 'http://localhost:3000/api/user/register')
         .then((response) => {
-            expect(response.body).to.contain('Register')
+            expect(response.status).to.equal(200)
         })
 
 })
