9b156a09b3ffe21d9156bafc94327ab2b35a5f09
elinordin <elin.evelina@live.se>
2021-08-19 10:29:29 +0200

    code cleanup

--- project-3-auth-api/cypress/integration/auth.spec.js
+++ project-3-auth-api/cypress/integration/auth.spec.js
@@ -1,11 +1,13 @@
 describe("/register test suite", () => {
-  it("returns 200 when sending in a body", () => {
+  const registerEndpoint = "http://localhost:3000/api/user/register"
+
+  it("creates user with valid body", () => {
     const body = {
       name: "Name",
       email: "name@email.com",
       password: "password",
     }
-    cy.request("POST", "http://localhost:3000/api/user/register", body).then(
+    cy.request("POST", registerEndpoint, body).then(
       (response) => {
         expect(response.status).to.equal(200)
         expect(response.body.name).to.equal(body.name)
@@ -18,7 +20,7 @@ describe("/register test suite", () => {
   it("returns 400 when not sending in a body", () => {
     cy.request({
       method: "POST",
-      url: "http://localhost:3000/api/user/register",
+      url: registerEndpoint,
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.equal(400)
