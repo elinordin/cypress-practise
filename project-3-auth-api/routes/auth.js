cd4f0f6359969141e84fd99abe333712659a8422
elinordin <elin.evelina@live.se>
2021-08-19 10:55:08 +0200

    add validation with Joi

--- project-3-auth-api/routes/auth.js
+++ project-3-auth-api/routes/auth.js
@@ -1,7 +1,18 @@
 const router = require('express').Router()
 const User = require('../models/users')
+const Joi = require('joi')
+
+const validationSchema = Joi.object({
+    name: Joi.string().min(2).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+})
 
 router.post('/register', async (req, res) => {
+    const {error} = validationSchema.validate(req.body)
+
+    if (error) return res.status(400).send(error.details)
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
