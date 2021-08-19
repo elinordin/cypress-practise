18705a699e5cb668f98a0c2fc5716830ee4e0f26
elinordin <elin.evelina@live.se>
2021-08-19 10:05:49 +0200

    add positive and negative test to /register test suite

--- project-3-auth-api/cypress/integration/auth.spec.js
+++ project-3-auth-api/cypress/integration/auth.spec.js
@@ -1,8 +1,20 @@
-it('returns 200 with post-request to /register', () => {
+describe('/register test suite', () => {
 
-    cy.request('POST', 'http://localhost:3000/api/user/register')
-        .then((response) => {
-            expect(response.status).to.equal(200)
-        })
+    it('returns 200 when sending in a body', () => {
+        const body = { name: "Name", email: "name@email.com", password: "password" }
+        cy.request('POST', 'http://localhost:3000/api/user/register', body)
+            .then((response) => {
+                expect(response.status).to.equal(200)
+            })
+    })
 
+    it('returns 400 when not sending in a body', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/api/user/register',
+            failOnStatusCode: false
+        }).then((response) => {
+                expect(response.status).to.equal(400)
+            })
+    })
 })
